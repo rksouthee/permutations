086c0c4613ce5c8c81d384c93c1754a86b179f6c
Robert Southee <rksouthee@gmail.com>
2021-02-13 17:46:07 +0000

    Add example for unstable remove algorithm

--- main.cpp
+++ main.cpp
@@ -47,9 +47,30 @@ void test_inshuffle()
     std::cout << '\n';
 }
 
+void remove_if_example(bool stable)
+{
+    int a[] = { 1, 3, 2, 4, 7, 5, 6, 9, 8, 0 };
+    int* last = std::end(a);
+    const auto p = [] (int x) { return x % 2 == 0; };
+    if (stable)
+        last = std::remove_if(std::begin(a), last, p);
+    else
+        last = ::remove_if_unstable(std::begin(a), last, p);
+
+    std::copy(a, last, std::ostream_iterator<int>(std::cout, " "));
+}
+
 int main()
 {
     test_partition_stable_position();
     test_unweave();
     test_inshuffle();
+
+    std::cout << "\nremove stable\n";
+    remove_if_example(true);
+
+    std::cout << "\nremove unstable\n";
+    remove_if_example(false);
+
+    std::cout << '\n';
 }
